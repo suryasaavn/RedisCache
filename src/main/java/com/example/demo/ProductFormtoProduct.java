149b7864c34c981cbdd95f69e71ad572d97296b1
Suryasaavn <iamsurya621@gmail.com>
2017-08-09 12:14:18 -0500

    updated commit

---
+++ src/main/java/com/example/demo/ProductFormtoProduct.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+
+
+@Component
+public class ProductFormtoProduct implements Converter<ProductForm, Product> {
+
+    @Override
+    public Product convert(ProductForm productForm) {
+        Product product = new Product();
+        if (productForm.getId() != null  && !StringUtils.isEmpty(productForm.getId())) {
+            product.setId(productForm.getId());
+        }
+        product.setDescription(productForm.getDescription());
+        product.setPrice(productForm.getPrice());
+        product.setImageUrl(productForm.getImageUrl());
+        return product;
+    }
+}
---
+++ src/main/java/com/example/demo/ProductService.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import java.util.List;
+
+public interface ProductService {
+
+    List<Product> listAll();
+
+    Product getById(String id);
+
+    Product saveOrUpdate(Product product);
+
+    void delete(String id);
+
+    Product saveOrUpdateProductForm(ProductForm productForm);
+}
+
